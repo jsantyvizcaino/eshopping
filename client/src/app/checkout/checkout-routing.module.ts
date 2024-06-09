ea9c4a59d85a121797f10a51bb0ceae019b7ab6c
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2024-06-09 12:49:20 -0500

    [ADD]: client

--- client/src/app/checkout/checkout-routing.module.ts
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { BasketComponent } from './basket.component';
+import { CheckoutComponent } from './checkout.component';
 
-const routes: Routes=[
-  {path:'',component:BasketComponent}
+const routes: Routes = [
+  {path: '', component: CheckoutComponent}
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     RouterModule.forChild(routes)
-
   ],
   exports:[
     RouterModule
   ]
 })
-export class BasketRoutingModule { }
+export class CheckoutRoutingModule { }
